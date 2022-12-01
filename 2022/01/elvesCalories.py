01f76f86eb58effd51eb845834411c61967b2a8a
Inzaguiz <audran83@live.fr>
2022-12-01 11:53:01 +0100

    2022 - 1st day - Part 2

--- 2022/01/elvesCalories.py
+++ 2022/01/elvesCalories.py
@@ -1,11 +1,13 @@
-maxCal = 0
-cal = 0
 with open("2022/01/input.txt", "r") as f:
+    sortedElves = []
+    cal = 0
     for line in f: 
         if (len(line) > 1):
             cal += int(line)
         else:
-            maxCal = maxCal if maxCal > cal else cal
+            sortedElves.append(cal)
             cal = 0
     else: 
-        print("The elf carrying the most calories have:", maxCal)
+        sortedElves.sort(reverse=True)
+        print("The elf who is carrying the most have " + str(sortedElves[0]) + " Calories.")
+        print("The top three of elves are carrying " + str(sortedElves[0] + sortedElves[1] + sortedElves[2]) + " in total.")
