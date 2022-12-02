8009727cdac20d01c847d2b4c23c7b97475734b3
Inzaguiz <audran83@live.fr>
2022-12-02 02:34:57 +0100

    2021 - 3rd day - WIP

---
+++ 2021/03/binDiag.py
@@ -0,0 +1,32 @@
+def transpose(matrix):
+    output = []
+    for index, row in enumerate(matrix):
+        if (index == 0):
+            for col in row:
+                output.append([col])
+        else :
+            for index, col in enumerate(row):
+                output[index].append(col)
+    return output
+
+with open("2021/03/input.txt", "r") as f:
+    binaryTable = []
+    trans_binaryTable = []
+    
+    for line in f:
+        binaryTable.append([*line])
+    trans_binaryTable = transpose(binaryTable)
+    
+gamma = ""
+epsilon = ""
+for a in trans_binaryTable:
+    if a.count("0") > a.count("1"):
+        gamma += "0"
+        epsilon += "1"
+    else:
+        gamma += "1"
+        epsilon += "0"
+
+print(gamma)
+print(epsilon)
+print(int(gamma, 2) * int(epsilon, 2))
