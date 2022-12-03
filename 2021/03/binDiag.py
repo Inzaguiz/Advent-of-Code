371f60efd984b7ed4b556012a6122f15e791c6de
Inzaguiz <audran83@live.fr>
2022-12-03 01:36:23 +0100

    2021 - 3rd day - better part 1

--- 2021/03/binDiag.py
+++ 2021/03/binDiag.py
@@ -1,33 +1,23 @@
-def transpose(matrix):
-    output = []
-    for index, row in enumerate(matrix):
-        row.pop()
-        if (index == 0):
-            for col in row:
-                output.append([col])
-        else :
-            for index, col in enumerate(row):
-                output[index].append(col)
-    return output
-
 with open("2021/03/input.txt", "r") as f:
     binaryTable = []
-    trans_binaryTable = []
-    
     for line in f:
-        binaryTable.append([*line])
-    trans_binaryTable = transpose(binaryTable)
+        l = [*line]
+        l.pop()
+        binaryTable.append(l)
     
 gamma = ""
 epsilon = ""
-for a in trans_binaryTable:
-    if a.count("0") > a.count("1"):
-        gamma += "0"
-        epsilon += "1"
-    else:
+print(len(binaryTable))
+for i in range(len(binaryTable[0])):
+    bitAtIndex = []
+    for j in binaryTable:
+        bitAtIndex.append(j[i])
+    if bitAtIndex.count("1") > bitAtIndex.count("0"):
         gamma += "1"
         epsilon += "0"
-
+    else : 
+        gamma += "0"
+        epsilon += "1"
 print(gamma)
 print(epsilon)
 print(int(gamma, 2) * int(epsilon, 2))
