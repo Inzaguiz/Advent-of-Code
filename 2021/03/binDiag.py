c4fd5cddb9d06dbc5356480d687580df6081125e
Inzaguiz <audran83@live.fr>
2022-12-03 02:18:37 +0100

    2021 - 3rd day - finished

--- 2021/03/binDiag.py
+++ 2021/03/binDiag.py
@@ -4,7 +4,35 @@ with open("2021/03/input.txt", "r") as f:
         l = [*line]
         l.pop()
         binaryTable.append(l)
+   
+   
+def rating(table, i, type):
+    bitAtIndex = []
+    output = []
+    for j in table:
+        bitAtIndex.append(j[i])
     
+    for j in table:
+        if type == "oxygen":
+            if bitAtIndex.count("1") >= bitAtIndex.count("0"):
+                if j[i] == "1":
+                    output.append(j)
+            else : 
+                if j[i] == "0":
+                    output.append(j)
+        if type == "co2":
+            if bitAtIndex.count("1") < bitAtIndex.count("0"):
+                if j[i] == "1":
+                    output.append(j)
+            else : 
+                if j[i] == "0":
+                    output.append(j)
+                
+    if (len(output) == 1 or i == len(table[0])):
+        return "".join(output[0])
+    else:
+        return rating(output, i + 1, type)
+
 gamma = ""
 epsilon = ""
 for i in range(len(binaryTable[0])):
@@ -17,6 +45,9 @@ for i in range(len(binaryTable[0])):
     else : 
         gamma += "0"
         epsilon += "1"
-print(gamma)
-print(epsilon)
-print(int(gamma, 2) * int(epsilon, 2))
+print("consumption:", int(gamma, 2) * int(epsilon, 2))
+
+oxygen = rating(binaryTable, 0, "oxygen")
+co2 = rating(binaryTable, 0, "co2")
+
+print("life support:", int(oxygen, 2) * int(co2, 2))
